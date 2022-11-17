5881b56067723ecc4ef4e14971a1bcaa343a2166
scottiepowell <scottiepowell@gmail.com>
2022-11-17 06:04:38 -0700

    17NOV-app-nginx

--- oci-infra/infra.tf
+++ oci-infra/infra.tf
@@ -231,3 +231,10 @@ resource "kubernetes_service" "nginx_service" {
     type = "NodePort"
   }
 }
+resource "oci_artifacts_container_repository" "docker_repository" {
+  compartment_id = var.compartment_id
+  display_name   = "free-kubernetes-nginx"
+
+  is_immutable = false
+  is_public    = false
+}
